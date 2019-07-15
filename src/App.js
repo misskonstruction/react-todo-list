0f35cdda55f2fa7518d43fe970a8347b29230fd1
misskonstruction <misskontruction@gmail.com>
2019-07-15 19:18:51 -0400

    finished testing suite

--- src/App.js
+++ src/App.js
@@ -56,10 +56,11 @@ function App() {
 
   function removeTodoAtIndex(i) {
     if (i === 0 && todos.length === 1) return;
+    
     setTodos(todos => todos.slice(0, i).concat(todos.slice(i + 1, todos.length)));
     setTimeout(() => {
       document.forms[0].elements[i - 1].focus();
-    }, 0);
+    },0);
   }
 
   function toggleTodoCompleteAtIndex(index) {
